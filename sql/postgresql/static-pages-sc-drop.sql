9ddf09f6909b9495ddd964ba71ec8618db93b3a0
Dave Bauer <dave@thedesignexperience.org>
2001-10-30 20:59:10 +0000

    more adds

---
+++ sql/postgresql/static-pages-sc-drop.sql
@@ -0,0 +1,7 @@
+-- Dave Bauer dave@thedesignexperience.org
+-- 2001-10-27
+
+select acs_sc_impl__delete(
+	   'FtsContentProvider',		-- impl_contract_name
+           'static_page'				-- impl_name
+);
