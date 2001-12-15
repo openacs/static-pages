c127938f3635cf86de983e5189eb37e1e78b22c2
Dave Bauer <dave@thedesignexperience.org>
2001-12-15 01:39:01 +0000

    added support for templating

--- tcl/static-pages-init.tcl
+++ tcl/static-pages-init.tcl
@@ -5,5 +5,9 @@ if ![nsv_exists CR_LOCATIONS STATIC_PAGES] {
     nsv_set CR_LOCATIONS STATIC_PAGES "[file dirname [string trimright [ns_info tcllib] "/"]]/www"
 }
 
+if ![nsv_exists static_pages package_id] {
+    nsv_set static_pages package_id [apm_package_id_from_key static-pages]
+}
+
 ns_log notice "static-pages-init.tcl loaded"
     
