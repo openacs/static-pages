f1316d2763a37a6e3074769e92af2d39f4c1c81d
Jeff Davis <davis@xarg.net>
2002-09-12 22:44:31 +0000

    add missing ad_library calls

--- tcl/static-pages-sc-procs.tcl
+++ tcl/static-pages-sc-procs.tcl
@@ -1,21 +1,27 @@
-# static-pages/tcl/static-pages-sc-procs.tcl
-# implements OpenFTS Search service contracts
-# Dave Bauer dave@thedesignexperience.org
-# 2001-10-27
+ad_library { 
+    static-pages/tcl/static-pages-sc-procs.tcl
+    implements OpenFTS Search service contracts
 
-# Right now I am leaving the keywords blank
-# in the future we should either extract them from the META keyword tag
-# or allow assignment of cr_keywords to static_pages
+    @author Dave Bauer (dave@thedesignexperience.org)
+    @creation-date 2001-10-27
+    @cvs-id $Id$
+}
 
 ad_proc static_page__datasource {
     object_id
 } {
+    Right now I am leaving the keywords blank
+    in the future we should either extract them from the META keyword tag
+    or allow assignment of cr_keywords to static_pages
+    
     @author Dave Bauer (dave@thedesignexperience.org)
-} {
 
+    @param object_id the object_id for which to generate the data
+
+} {
     set path_stub [cr_fs_path STATIC_PAGES]
     
-    db_0or1row sp_datasource ""     -column_array datasource
+    db_0or1row sp_datasource "" -column_array datasource
 
     return [array get datasource]
 }
